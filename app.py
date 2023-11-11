f1a4366d40b24ef860f5e557ca1c0f75cfc44e79
HugoItaloMC <magalhaeshugocorreia@gmail.com>
2023-11-11 10:17:32 -0300

    FEAT implementando funcoes/objetos: Iniciando principal funcionalidade da aplicacão

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
+# Execucão da aplicacão, gerando arquivo com base e retorna seu caminho
+
+
+# Imports
 import os
+import csv
+
+from src.run import Task
+
+
+def asset() -> str:
+    work_dir = os.path.join(os.environ['HOME'], os.getcwd())
+    return work_dir
+
 
 if __name__ == '__main__':
-    print("%s" % os.name)
+    print("%s" % asset())  # Test current dir
