94d5af5269a6f651950838df79240d7348cc7894
HugoItaloMC <magalhaeshugocorreia@gmail.com>
2023-11-13 11:45:47 -0300

    TEST testes na aplicacão: Teste unitário no arquivo queue.py que contém corotinas importantes para aplicacão que vão executar tarefas do módulo pipe.py instânciando seus objetos e executando suas corotinas

---
+++ tests/test_helper.py
@@ -0,0 +1,7 @@
+async def pool_queue(queue, _class):
+    await queue.put(_class)
+
+
+async def mk_csv_file(path):
+    print("Atributo Recebido %s " % path)
+    print("Chamando corotina mk_csv_file do módulo helper.py")
